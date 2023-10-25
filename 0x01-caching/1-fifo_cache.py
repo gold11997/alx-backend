6809abb4ad5f1743d23dfd26a2c3e11907939ddb
gold11997 <abrahambenson90@gmail.com>
2023-10-25 22:48:39 +0100

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,22 +1,32 @@
-#!/usr/bin/env python3
-"""class BasicCache that inherits from BaseCaching and is a caching system
-"""
+#!/usr/bin/python3
+""" FIFOCache module """
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-from base_caching import BaseCaching
+class FIFOCache(BaseCaching):
+    """Implements the FIFO replacement policy"""
+    def __init__(self) -> None:
+        super().__init__()
+        self.keys_queue = []
 
-
-class BasicCache(BaseCaching):
-    """ - dictionary from the parent class BaseCaching
-    """
     def put(self, key, item):
-        """assign to the dictionary to cache
-        """
-        if key is None or item is None:
-            return
+        """assign to the dictionary self.cache_data the
+        item value for the key key"""
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        else:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # remove old item from the cache
+                old_key = self.keys_queue.pop(0)
+                print(f"DISCARD: {old_key}", end='\n')
+                del self.cache_data[old_key]
+        # append the new item to tha cached_data
         self.cache_data[key] = item
+        self.keys_queue.append(key)
 
     def get(self, key):
-        """return the value in cache
-        """
-        return self.cache_data.get(key, None)
+        """Returns the value in self.cache_data linked to the key """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
