491781b97e8e5fff4e99c76b7a2ed5100feb9b57
gold11997 <abrahambenson90@gmail.com>
2023-10-25 22:43:35 +0100

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,26 @@
-#!/usr/bin/env python3
-"""
-BasicCache module
-"""
-
-from base_caching import BaseCaching
+#!/usr/bin/python3
+""" BasicCache module """
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    BasicCache inherits from BaseCaching and is a caching system
-    """
+    """ BasicCache class"""
+    def __init__(self):
+        """BasicCache constructor, inherits from BaseCaching"""
+        super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None and item is not None:
+        """ assigns to the dictionary self.cache_data the
+            item value for the key key.
+            If key or item is None, this method should not do anything.
+        """
+        if key not in self.cache_data and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """Returns the value in self.cache_data linked to the key """
+        if key not in self.cache_data:
+            return None
+        if self.cache_data[key] is None:
+            return None
+        return self.cache_data[key]
